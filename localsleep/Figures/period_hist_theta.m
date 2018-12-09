4796d83f4c8e571b3ddccc754374d0bf946cea67
Andrew-Y-1 <45579945+Andrew-Y-1@users.noreply.github.com>
2018-12-10 10:20:26 +1100

    Add files via upload

--- localsleep/Figures/period_hist_theta.m
+++ localsleep/Figures/period_hist_theta.m
@@ -22,7 +22,7 @@ bsl_files=dir([eeg_path filesep 'probe_nfEEG_S3*.mat']);
 %% loop across trials for baseline blocks
 all_amp_Waves=[];
 histc_record = zeros(20,51); % Documenting purposes; rows = participants, columns = bin/counts
-
+collect_all=[];
 
 for n=1:length(bsl_files)
     % load behavioural results
@@ -31,7 +31,7 @@ for n=1:length(bsl_files)
     load([eeg_path filesep 'wanderIM_twa2_' SubID])
     
     all_Waves = double(all_Waves); 
-    thr_wave = prctile(all_Waves(all_Waves(:,3)==2,4),90);
+    thr_wave = prctile(all_Waves(all_Waves(:,3)==2,4),80);
     Fz_Waves = all_Waves(all_Waves(:,3)==2,:);
     halfw_samples = Fz_Waves(Fz_Waves(:,4)>=thr_wave,6) - Fz_Waves(Fz_Waves(:,4)>=thr_wave,5);
     % ^ Half-period in samples --> convert to period in Hz
@@ -40,7 +40,8 @@ for n=1:length(bsl_files)
     [N edges] = histcounts(period,0:0.1:5.1); % output 'edges' includes rightmost bin
     histc_record(n,:)= N;
     N=(N./(sum(N))*100); % convert to percentage
-    % 20 participants, 5 by 4 subplotting   
+    % 20 participants, 5 by 4 subplotting
+    collect_all = [collect_all ; N];
     subplot(5,4,n)
     xlabel(SubID);
     plot(edges(1:length(edges)-1),N);
@@ -51,10 +52,65 @@ end
 
 suplabel('Period (Hz)','x');
 suplabel('Percentage of Waves (%)','y');
-suplabel('Period of Theta Waves (90) of Fz (Ch2)','t');
+suplabel('Period of Theta Waves (80) of Fz (Ch2)','t');
 
 
 
+
+
+%% Generate Plot for Meanplot
+figure;
+mean_all = mean(collect_all);
+plot(edges(1:length(edges)-1),mean_all);
+axis([0 5 0 10])
+
+suplabel('Period (Hz)','x');
+suplabel('Percentage of Waves (%)','y');
+suplabel('Mean Period of Theta (80) Waves of Fz (Ch2)','t');
+%% Corplot for period and p2p amplitude 
+clear all;
+close all;
+run ../localdef_wanderIM
+
+% adding relevant toolboxes to the path
+% spm12 and LSCPtools
+% addpath(genpath(spm12_path))
+addpath(genpath(lscpTools_path))
+% addpath(path_localsleep)
+
+% select relevant files, here baseline blocks
+eeg_path=[root_path filesep 'preproc_eeg'];
+behav_path=[root_path filesep 'behav'];
+bsl_files=dir([eeg_path filesep 'probe_nfEEG_S3*.mat']);
+
+all_amp_Waves=[];
+histc_record = zeros(20,51); % Documenting purposes; rows = participants, columns = bin/counts
+all_period=[];
+all_p2p=[];
+
+for n=1:length(bsl_files)
+    % load behavioural results
+    SubID=bsl_files(n).name;
+    SubID=SubID(findstr(SubID,'_S3')+2:findstr(SubID,'_S3')+4);
+    load([eeg_path filesep 'wanderIM_twa2_' SubID])
+    
+    all_Waves = double(all_Waves); 
+    thr_wave = prctile(all_Waves(all_Waves(:,3)==2,4),80);
+    Fz_Waves = all_Waves(all_Waves(:,3)==2,:);
+    halfw_samples = Fz_Waves(Fz_Waves(:,4)>=thr_wave,6) - Fz_Waves(Fz_Waves(:,4)>=thr_wave,5);
+  
+    % ^ Half-period in samples --> convert to period in Hz
+    period = 1./(halfw_samples*2/500);
+    all_period = [all_period ; period];
+    all_p2p = [all_p2p; Fz_Waves(Fz_Waves(:,4)>=thr_wave,4)];
+       
+end
+
+
+simpleCorPlot(all_period,all_p2p,[],'pearson');
+xlabel('Period (Hz)')
+ylabel('Peak-to-peak Amplitude')
+title('Correlation between Period of Theta(80) Waves and Peak-to-peak Amplitude in Fz(Ch2)')
 
 
 
