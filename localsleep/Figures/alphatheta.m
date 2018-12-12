6f2a6eea75f8e5b5a191e7e4b207e5c4eb4c283d
Andrew-Y-1 <45579945+Andrew-Y-1@users.noreply.github.com>
2018-12-12 11:57:24 +1100

    Add files via upload

--- localsleep/Figures/alphatheta.m
+++ localsleep/Figures/alphatheta.m
@@ -53,12 +53,131 @@ for n=1:length(bsl_files) % for each participant (generate a topography of corre
             end
         end
         
-        save([eeg_path filesep 'at_ratio_wanderIM_twa2_' SubID],'a_t_all')
+        save([eeg_path filesep 'at_ratio_wanderIM_twa2_' SubID],'a_t_all') % save 63 (channel) by 60 (probe) matrix of a/t ratio
     
     end
     
+   
+%% alphatheta next step
+
+
+clear all;
+% close all;
+run ../localdef_wanderIM
+
+% adding relevant toolboxes to the path
+% spm12 and LSCPtools
+addpath(genpath(spm12_path))
+addpath(genpath(lscpTools_path))
+% addpath(path_localsleep)
+
+% select relevant files, here baseline blocks
+eeg_path=[root_path filesep 'preproc_eeg'];
+behav_path=[root_path filesep 'behav'];
+bsl_files=dir([eeg_path filesep 'probe_nfEEG_S3*.mat']);
+
+all_corrs = zeros(63,20);
+
+for n=1:length(bsl_files)
+    SubID=bsl_files(n).name;
+    SubID=SubID(findstr(SubID,'_S3')+2:findstr(SubID,'_S3')+4);    
+    behav_file=dir([behav_path filesep 'wanderIM_behavres_s' SubID '_*.mat']);
+    load([behav_path filesep behav_file.name]);
+    load([eeg_path filesep 'at_ratio_wanderIM_twa2_' SubID])
+    for nE = 1:63
+        a = a_t_all(nE,:)'; % a/t ratio
+        b = probe_res(:,38); % sleepiness rating
+        rho = corr(a,b); % correlation of a/t ratio and sleepiness rating
+        all_corrs(nE,n) = rho; % 63 (channel) by 20 (participants) matrix of correlation coefficients
+    end
     
-    %% Original Data
+end
+
+
+%% Generate Topographies for Alpha-Theta Ratio
+
+figure;
+format_fig;
+addpath(genpath(path_eeglab));
+temp_topo=mean(all_corrs,2); % vector of 63 values
+topoplot(temp_topo, layout.chaninfo,'style','map','whitebk','on','electrodes','on');
+cmap=colormap('parula'); %cmap=flipud(cmap); colormap(cmap);
+% caxis([0 2.5])
+rmpath(genpath(path_eeglab));
+colorbar;
+caxis([0 max(temp_topo)])
+ 
+%% ERP
+clear all;
+% close all;
+run ../localdef_wanderIM
+figure
+
+% adding relevant toolboxes to the path
+% spm12 and LSCPtools
+addpath(genpath(spm12_path))
+addpath(genpath(lscpTools_path))
+% addpath(path_localsleep)
+
+% select relevant files, here baseline blocks
+eeg_path=[root_path filesep 'preproc_eeg'];
+behav_path=[root_path filesep 'behav'];
+bsl_files=dir([eeg_path filesep 'probe_nfEEG_S3*.mat']);
+
+all_amp_Waves=[];
+erp_all=[];
+for n=1:length(bsl_files) % for each participant (generate a topography of correlation coefficients)
+    
+    filename=bsl_files(n).name;
+    D=spm_eeg_load([eeg_path filesep filename]);
+    fprintf('... processing subject %s\n',D.fname)
+    
+    
+    % load behavioural results
+    SubID=D.fname;
+    SubID=SubID(findstr(SubID,'_S3')+2:findstr(SubID,'_S3')+4);
+    behav_file=dir([behav_path filesep 'wanderIM_behavres_s' SubID '_*.mat']);
+    
+    load([behav_path filesep behav_file.name]);
+    
+%     left_freq(n)=SubjectInfo.FlickerR; % CAREFUL this is inverted
+%     right_freq(n)=SubjectInfo.FlickerL;
+    
+    these_times=D.indsample(-20):D.indsample(0)-1;
+    temp_data=D(1:63,these_times,:); % D contains the data with channels * time * trials
+    temp_data=temp_data-repmat(mean(temp_data([10 21],:,:),1),[size(temp_data,1) 1 1]); % D contains the data with channels * time * trials
+    load([eeg_path filesep 'wanderIM_twa2_' SubID])
+    
+    fz_waves = all_Waves(all_Waves(:,3)==2,:);
+    thr_wave=prctile(all_Waves(all_Waves(:,3)==2,4),80);
+    negzx_all = fz_waves(fz_waves(:,4)>=thr_wave,5);
+    npr_all = fz_waves(fz_waves(:,4)>=thr_wave,2);
+    npr_all(negzx_all>=9250 | negzx_all<=750)=[];
+    negzx_all(negzx_all>=9250)=[];
+    negzx_all(negzx_all<=750)=[];
+    
+    
+    
+    
+    erp=[];
+    for npr = 1:length(npr_all)
+           temp =  temp_data(2,negzx_all(npr)-750:negzx_all(npr)+750,npr_all(npr));
+           erp = [erp; temp];
+    end
+    
+    subplot(5,4,n);
+    simpleTplot(-1.5:0.002:1.5, mean(erp));
+    xlabel(SubID);
+    erp_all=[erp_all; erp];
+    
+end
+
+suplabel('Samples (Fs=500)', 'x'); 
+suplabel('Amplitude', 'y');
+title('ERP for Theta(80) Events for Fz(Ch2) for each participant');
+
+ 
+    %% Original Data; period marking
    close all
    thr_Wave = thr_Wave1
    fz_waves = all_Waves(all_Waves(:,3)==2,:);
@@ -95,120 +214,5 @@ plot(e,'-o','MarkerIndices',[1 bb(n)-aa(n)+1]);
 
 nn=nn+2;
 end
-%% alphatheta next step
-
-
-clear all;
-% close all;
-run ../localdef_wanderIM
-
-% adding relevant toolboxes to the path
-% spm12 and LSCPtools
-addpath(genpath(spm12_path))
-addpath(genpath(lscpTools_path))
-% addpath(path_localsleep)
-
-% select relevant files, here baseline blocks
-eeg_path=[root_path filesep 'preproc_eeg'];
-behav_path=[root_path filesep 'behav'];
-bsl_files=dir([eeg_path filesep 'probe_nfEEG_S3*.mat']);
-
-all_corrs = zeros(63,60);
-
-for n=1:length(bsl_files)
-    SubID=bsl_files(n).name;
-    SubID=SubID(findstr(SubID,'_S3')+2:findstr(SubID,'_S3')+4);    
-    behav_file=dir([behav_path filesep 'wanderIM_behavres_s' SubID '_*.mat']);
-    load([behav_path filesep behav_file.name]);
-    load([eeg_path filesep 'at_ratio_wanderIM_twa2_' SubID])
-    for nE = 1:63
-        a = a_t_all(nE,:)';
-        b = probe_res(:,38);
-        rho = corr(a,b);
-        all_corrs(nE,n) = rho;
-    end
-    
-end
-
-
-%% Generate Topographies
-
-figure;
-format_fig;
-addpath(genpath(path_eeglab));
-temp_topo=mean(all_corrs.*100,2); % vector of 63 values
-topoplot(temp_topo, layout.chaninfo,'style','map','whitebk','on','electrodes','on');
-cmap=colormap('parula'); %cmap=flipud(cmap); colormap(cmap);
-% caxis([0 2.5])
-rmpath(genpath(path_eeglab));
-colorbar;
-caxis([0 max(temp_topo)])
- 
-%% ERP
-clear all;
-% close all;
-run ../localdef_wanderIM
-figure
-
-% adding relevant toolboxes to the path
-% spm12 and LSCPtools
-addpath(genpath(spm12_path))
-addpath(genpath(lscpTools_path))
-% addpath(path_localsleep)
-
-% select relevant files, here baseline blocks
-eeg_path=[root_path filesep 'preproc_eeg'];
-behav_path=[root_path filesep 'behav'];
-bsl_files=dir([eeg_path filesep 'probe_nfEEG_S3*.mat']);
-
-all_amp_Waves=[];
-for n=1:length(bsl_files) % for each participant (generate a topography of correlation coefficients)
-    
-    filename=bsl_files(n).name;
-    D=spm_eeg_load([eeg_path filesep filename]);
-    fprintf('... processing subject %s\n',D.fname)
-    
-    
-    % load behavioural results
-    SubID=D.fname;
-    SubID=SubID(findstr(SubID,'_S3')+2:findstr(SubID,'_S3')+4);
-    behav_file=dir([behav_path filesep 'wanderIM_behavres_s' SubID '_*.mat']);
-    
-    load([behav_path filesep behav_file.name]);
-    
-%     left_freq(n)=SubjectInfo.FlickerR; % CAREFUL this is inverted
-%     right_freq(n)=SubjectInfo.FlickerL;
-    
-    these_times=D.indsample(-20):D.indsample(0)-1;
-    temp_data=D(1:63,these_times,:); % D contains the data with channels * time * trials
-    temp_data=temp_data-repmat(mean(temp_data([10 21],:,:),1),[size(temp_data,1) 1 1]); % D contains the data with channels * time * trials
-    load([eeg_path filesep 'wanderIM_twa2_' SubID])
-    
-    fz_waves = all_Waves(all_Waves(:,3)==2,:);
-    thr_wave=prctile(all_Waves(all_Waves(:,3)==2,4),90);
-    negzx_all = fz_waves(fz_waves(:,4)>=thr_wave,5);
-    npr_all = fz_waves(fz_waves(:,4)>=thr_wave,2);
-    npr_all(negzx_all>=9250 | negzx_all<=750)=[];
-    negzx_all(negzx_all>=9250)=[];
-    negzx_all(negzx_all<=750)=[];
-    
-    
-    
-    
-    erp=[];
-    for npr = 1:length(npr_all)
-           temp =  temp_data(2,negzx_all(npr)-750:negzx_all(npr)+750,npr_all(npr));
-           erp = [erp; temp];
-    end
-    
-    subplot(5,4,n);
-    plot(mean(erp));
-    xlabel(SubID);
-    
-end
-
-suplabel('Samples (Fs=500)', 'x'); 
-suplabel('Amplitude', 'y');
-title('ERP for Theta(90) Events for Fz(Ch2) for each participant');
     
     
